5164ccdecb1e54368014c0f11e22073005641723
KamakshiAbbi <kamakshigit12@gmail.com>
2016-04-25 22:24:15 +0530

    ACD_JAVAB_Session2_Assignment3
    Printing a pattern

---
+++ ACD_JAVAB_Session2_Assignment3/src/Session2_Assignment3.java
@@ -0,0 +1,18 @@
+//Session2_Assignment3 : 24.4.2016
+//Author: Kamakshi Abbi
+public class Session2_Assignment3 {
+	public static void main(String[] args) {
+		for (int i = 1; i < 6; i++) {
+			for (int j = 1; j < i; j++) {
+				System.out.print(j);
+			}
+			System.out.println(i);	
+		}
+		for (int i = 4; i >= 1; i--) {
+			for (int j = 1; j < i; j++) {
+				System.out.print(j);
+		}
+		System.out.println(i);	
+		}
+	}
+}
